97d4636831bee65483dd2e18df0caf32edd625d0
Abdullahi Aden <Abbe6152@gmail.com>
2021-05-07 15:05:57 +0200

    category and show working for men

--- v2/index.php
+++ v2/index.php
@@ -12,149 +12,184 @@ header("Referrer-Policy: no-referrer");
 
 
 $id = [
-    "bag" => 1,
-    "shirt" => 2,
-    "trouser" => 3,
-    "shorts" => 4,
-    "tie" => 5,
-    "t-shirt" => 6,
-    "shoes" => 7,
-    "sweater" => 8,
-    "jacket" => 9,
-    "coat" => 10,
-    "socks" => 11,
-    "tracksuit" => 12,
-    "dress" => 13,
-    "blouse" => 14,
-    "nighty" => 15,
-    "bikini" => 16,
-    "boots" => 17,
-    "cap" => 18,
-    "polo shirt" => 19,
-    "shawl" => 20,
+  "bag" => 1,
+  "shirt" => 2,
+  "trouser" => 3,
+  "shorts" => 4,
+  "tie" => 5,
+  "t-shirt" => 6,
+  "shoes" => 7,
+  "sweater" => 8,
+  "jacket" => 9,
+  "coat" => 10,
+  "socks" => 11,
+  "tracksuit" => 12,
+  "dress" => 13,
+  "blouse" => 14,
+  "nighty" => 15,
+  "bikini" => 16,
+  "boots" => 17,
+  "cap" => 18,
+  "polo shirt" => 19,
+  "shawl" => 20,
 ];
 
 $title = [
-    "bag",
-    "shirt",
-    "trouser",
-    "shorts",
-    "tie",
-    "t-shirt",
-    "shoes",
-    "sweater",
-    "jacket",
-    "coat",
-    "socks",
-    "tracksuit",
-    "dress",
-    "blouse",
-    "nighty",
-    "bikini",
-    "boots",
-    "cap",
-    "polo shirt",
-    "shawl"
+  "bag",
+  "shirt",
+  "trouser",
+  "shorts",
+  "tie",
+  "t-shirt",
+  "shoes",
+  "sweater",
+  "jacket",
+  "coat",
+  "socks",
+  "tracksuit",
+  "dress",
+  "blouse",
+  "nighty",
+  "bikini",
+  "boots",
+  "cap",
+  "polo shirt",
+  "shawl"
 ];
 
 
 $description = [
-    "bag" =>  "description for bag",
-    "shirt" => "description for  shirt",
-    "trouser" => "description for  trouser",
-    "shorts" => "description for  shorts",
-    "tie" => "description for  tie",
-    "t-shirt" => "description for  t-shirt",
-    "shoes" => "description for  shoes",
-    "sweater" => "description for  sweater",
-    "jacket" => "description for  jacket",
-    "coat" => "description for  coat",
-    "socks" => "description for  socks",
-    "tracksuit" => "description for  tracksuit",
-    "dress" => "description for  dress",
-    "blouse" => "description for  blouse",
-    "nighty" => "description for  nighty",
-    "bikini" => "description for  bikini",
-    "boots" => "description for  boots",
-    "cap" => "description for  cap",
-    "polo shirt" => " description for  polo shirt",
-    "shawl" => " description for  shawl"
+  "bag" =>  "description for bag",
+  "shirt" => "description for  shirt",
+  "trouser" => "description for  trouser",
+  "shorts" => "description for  shorts",
+  "tie" => "description for  tie",
+  "t-shirt" => "description for  t-shirt",
+  "shoes" => "description for  shoes",
+  "sweater" => "description for  sweater",
+  "jacket" => "description for  jacket",
+  "coat" => "description for  coat",
+  "socks" => "description for  socks",
+  "tracksuit" => "description for  tracksuit",
+  "dress" => "description for  dress",
+  "blouse" => "description for  blouse",
+  "nighty" => "description for  nighty",
+  "bikini" => "description for  bikini",
+  "boots" => "description for  boots",
+  "cap" => "description for  cap",
+  "polo shirt" => " description for  polo shirt",
+  "shawl" => " description for  shawl"
 
 ];
 
 $image = [
 
-    "bag" => "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRnTv_Mm0r42u_nXk_H5f-qvmyBBWfxERMknwwMsejstzSGAxzOF6RCmCp0ZGf0pOaTLyP9rBntFYU&usqp=CAc",
-    "shirt" => "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ4IDAR6sqFGWGgmoJfToNBVQTWfsnLqPM3rg6xOScKPn6bZzredEXUw4IOhhTDrZjyxGoAtrlNvw&usqp=CAc",
-    "trouser" => "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTGtOfVrK3TFsyK2t_ileBAlvAsNDih_XxRsZJt5a1lteCqgbURr52TIUDNh8vg2dOrJTkSB-_ZfXmp&usqp=CAc",
-    "shorts" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "tie" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "t-shirt" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "shoes" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "sweater" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "jacket" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "coat" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "socks" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "tracksuit" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "dress" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "blouse" => "blouse https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "nighty" => "nighty https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "bikini" => " biki https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "boots" => "boot https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "cap" => " cap  https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "polo shirt" => " polo sh https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "shawl" => "shw https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "bag" => "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRnTv_Mm0r42u_nXk_H5f-qvmyBBWfxERMknwwMsejstzSGAxzOF6RCmCp0ZGf0pOaTLyP9rBntFYU&usqp=CAc",
+  "shirt" => "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ4IDAR6sqFGWGgmoJfToNBVQTWfsnLqPM3rg6xOScKPn6bZzredEXUw4IOhhTDrZjyxGoAtrlNvw&usqp=CAc",
+  "trouser" => "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTGtOfVrK3TFsyK2t_ileBAlvAsNDih_XxRsZJt5a1lteCqgbURr52TIUDNh8vg2dOrJTkSB-_ZfXmp&usqp=CAc",
+  "shorts" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "tie" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "t-shirt" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "shoes" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "sweater" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "jacket" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "coat" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "socks" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "tracksuit" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "dress" => "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "blouse" => "blouse https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "nighty" => "nighty https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "bikini" => " biki https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "boots" => "boot https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "cap" => " cap  https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "polo shirt" => " polo sh https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "shawl" => "shw https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
 
 ];
 
 $price = [
-    "bag" => 109.95,
-    "shirt" => 19.95,
-    "trouser" => 10.95,
-    "shorts" => 102.95,
-    "tie" => 120.95,
-    "t-shirt" => 119.95,
-    "shoes" => 104.95,
-    "sweater" => 29.95,
-    "jacket" => 39.95,
-    "coat" => 10.95,
-    "socks" => 11.95,
-    "tracksuit" => 13.95,
-    "dress" => 16.95,
-    "blouse" => 15.9,
-    "nighty" => 18.9,
-    "bikini" => 34.5,
-    "boots" => 12.5,
-    "cap" => 9.95,
-    "polo shirt" => 89.95,
-    "shawl" => 19.5,
+  "bag" => 109.95,
+  "shirt" => 19.95,
+  "trouser" => 10.95,
+  "shorts" => 102.95,
+  "tie" => 120.95,
+  "t-shirt" => 119.95,
+  "shoes" => 104.95,
+  "sweater" => 29.95,
+  "jacket" => 39.95,
+  "coat" => 10.95,
+  "socks" => 11.95,
+  "tracksuit" => 13.95,
+  "dress" => 16.95,
+  "blouse" => 15.9,
+  "nighty" => 18.9,
+  "bikini" => 34.5,
+  "boots" => 12.5,
+  "cap" => 9.95,
+  "polo shirt" => 89.95,
+  "shawl" => 19.5,
 
 ];
 
 $category = [
-    "bag" => "women",
-    "shirt" => "men",
-    "trouser" => "men",
-    "shorts" => "men",
-    "tie" => "men",
-    "t-shirt" => "women",
-    "shoes" => "women",
-    "sweater" => "women",
-    "jacket" => "women",
-    "coat" => "women",
-    "socks" => "women",
-    "tracksuit" => "women",
-    "dress" => "women",
-    "blouse" => "women",
-    "nighty" => "women",
-    "bikini" => "women",
-    "boots" => "men",
-    "cap" => "men",
-    "polo shirt" => "men",
-    "shawl" => "women"
+  "bag" => "women",
+  "shirt" => "men",
+  "trouser" => "men",
+  "shorts" => "men",
+  "tie" => "men",
+  "t-shirt" => "women",
+  "shoes" => "women",
+  "sweater" => "women",
+  "jacket" => "women",
+  "coat" => "women",
+  "socks" => "women",
+  "tracksuit" => "women",
+  "dress" => "women",
+  "blouse" => "women",
+  "nighty" => "women",
+  "bikini" => "women",
+  "boots" => "men",
+  "cap" => "men",
+  "polo shirt" => "men",
+  "shawl" => "women"
 ];
 
+$men = [
+  "shirt",
+  "trouser",
+  "shorts",
+  "tie",
+  "boots",
+  "cap",
+  "polo shirt"
 
+];
+
+$menArray = [
+  "shirt" => "men",
+  "trouser" => "men",
+  "shorts" => "men",
+  "tie" => "men",
+  "boots" => "men",
+  "cap" => "men",
+  "polo shirt" => "men",
+];
+$womenArray = [
+  "bag" => "women",
+  "t-shirt" => "women",
+  "shoes" => "women",
+  "sweater" => "women",
+  "jacket" => "women",
+  "coat" => "women",
+  "socks" => "women",
+  "tracksuit" => "women",
+  "dress" => "women",
+  "blouse" => "women",
+  "nighty" => "women",
+  "bikini" => "women",
+  "shawl" => "women"
+
+];
 
 // spara  i en ny array
 $products = array();
@@ -163,25 +198,25 @@ $show = $_GET['show'] ?? 20;
 $categoryFilter = $_GET['category'] ? $_GET['category'] : false;
 
 if (!isset($_GET['show']) && !isset($_GET['category'])) {
-    for ($i = 0; $i < 20; $i++) {
+  for ($i = 0; $i < 20; $i++) {
 
-        $tit = $title[$i];
-        $ids = $id[$tit];
-        $desc = $description[$tit];
-        $img = $image[$tit];
-        $pri = $price[$tit];
-        $categ = $category[$tit];
+    $tit = $title[$i];
+    $ids = $id[$tit];
+    $desc = $description[$tit];
+    $img = $image[$tit];
+    $pri = $price[$tit];
+    $categ = $category[$tit];
 
-        $product = array(
-            "id" => $ids,
-            "title" => $tit,
-            "description" => $desc,
-            "image" => $img,
-            "price" => $pri,
-            "category" => $categ,
-        );
-        array_push($products, $product);
-    }
+    $product = array(
+      "id" => $ids,
+      "title" => $tit,
+      "description" => $desc,
+      "image" => $img,
+      "price" => $pri,
+      "category" => $categ,
+    );
+    array_push($products, $product);
+  }
 }
 
 
@@ -190,160 +225,143 @@ if (!isset($_GET['show']) && !isset($_GET['category'])) {
 
 if ($show && $categoryFilter) {
 
-    if (($show <= 0 || $show > 20) && ($categoryFilter !== "men" || $categoryFilter !== "women" || $categoryFilter !== "jewelery")) {
-        $product = array(
-            "Show" => "Show must be between 1 and 20",
-        );
-        array_push($products, $product);
-    }
+  if (($show <= 0 || $show > 20) && ($categoryFilter !== "men" || $categoryFilter !== "women" || $categoryFilter !== "jewelery")) {
+    $product = array(
+      "Show" => "Show must be between 1 and 20",
+    );
+    array_push($products, $product);
+  }
 }
 // * only if show is given 
 elseif (isset($_GET['show'])) {
 
-    if ($show <= 0 or $show > 20) {
-        $product = array(
-            "Show" => "Show must be between 1 and 20",
-        );
-        array_push($products, $product);
-    } else {
+  if ($show <= 0 or $show > 20) {
+    $product = array(
+      "Show" => "Show must be between 1 and 20",
+    );
+    array_push($products, $product);
+  } else {
 
 
-        for ($i = 0; $i < $show; $i++) {
+    for ($i = 0; $i < $show; $i++) {
+      $tit = $title[rand(0, count($title) - 1)];
+      $ids = $id[$tit];
+      $desc = $description[$tit];
+      $img = $image[$tit];
+      $pri = $price[$tit];
+      $categ = $category[$tit];
 
+      $product = array(
+        "id" => $ids,
+        "title" => $tit,
+        "description" => $desc,
+        "image" => $img,
+        "price" => $pri,
+        "category" => $categ,
+      );
 
-            $tit = $title[rand(0, count($title) - 1)];
-            $ids = $id[$tit];
-            $desc = $description[$tit];
-            $img = $image[$tit];
-            $pri = $price[$tit];
-            $categ = $category[$tit];
-
-            $product = array(
-                "id" => $ids,
-                "title" => $tit,
-                "description" => $desc,
-                "image" => $img,
-                "price" => $pri,
-                "category" => $categ,
-            );
-
-            array_push($products, $product);
-        }
+      array_push($products, $product);
     }
+  }
 }
 
 // * category filter
 
 if ($categoryFilter) {
-    if (!in_array($categoryFilter, $category)) {
+  if (!in_array($categoryFilter, $category)) {
+
+    $product = array(
+      "Category" => "Category not found"
+    );
+    array_push($products, $product);
+  }
+
+
+  // * for men  
+  elseif ($categoryFilter === "men") {
+    if ($show <= count($category) && $show > 0) {
+
+      for ($i = 0; $i < $show; $i++) {
+        $tit = $men[$i];
+        $ids = $id[$tit];
+        $desc = $description[$tit];
+        $img = $image[$tit];
+        $pri = $price[$tit];
+        $categ = $menArray[$tit];
 
         $product = array(
-            "Category" => "Category not found"
+          "id" => $ids,
+          "title" => $tit,
+          "description" => $desc,
+          "image" => $img,
+          "price" => $pri,
+          "category" => $categ,
         );
-        array_push($products, $product);
-    }
-
-
-    // * for men  
-    elseif ($categoryFilter === "men") {
-
-        // * handle show limit (1-20) with catgeory  **** 
-        // TODO ----- limit how much to show from products when show is given 
-        if ($show <= count($category) && $show > 0) {
-
-
-            // for ($i = 0; $i < count($category); $i++) {
-
-            //     // $tit = $title[rand(0, count($title) - 1)];
-            //     $tit = $title[$i];
-            //     $ids = $id[$tit];
-            //     $desc = $description[$tit];
-            //     $img = $image[$tit];
-            //     $pri = $price[$tit];
-            //     $categ = $category[$tit];
-
-            //     if ($categ === "men") {
-            //         if ($i < $show) {
-            //             $product = array(
-            //                 "id" => $ids,
-            //                 "title" => $tit,
-            //                 "description" => $desc,
-            //                 "image" => $img,
-            //                 "price" => $pri,
-            //                 "category" => $categ,
-            //             );
-
-            //             // while ($show ) {
-            //             array_push($products, $product);
-            //         }
-
-            //         shuffle($products);
-            //     }
-            // }
-            for ($i = 0; $i <= $show; $i++) {
-                $tit = $title[$i];
-                // for ($i = 0; $i < count($category); $i++) {
-
-                // $tit = $title[rand(0, count($title) - 1)];
-                $ids = $id[$tit];
-                $desc = $description[$tit];
-                $img = $image[$tit];
-                $pri = $price[$tit];
-                $categ = $category[$tit];
-
-                if ($categ === "men") {
-                    $product = array(
-                        "id" => $ids,
-                        "title" => $tit,
-                        "description" => $desc,
-                        "image" => $img,
-                        "price" => $pri,
-                        "category" => $categ,
-                    );
-
-                    // * only push if categ is men '
-                    // * keep populting 
-
-                    array_push($products, $product);
-                    shuffle($products);
-                }
-
-                // }
-            }
+        if ($product['category'] == "men") {
+          array_push($products, $product);
+          shuffle($products);
         }
+      }
     }
+  }
 
 
-    // * for women 
-    elseif ($categoryFilter === "women") {
+  // * for women --- NOT WORKING FOR WOMEN
+  elseif ($categoryFilter === "women") {
 
-        //  handle show limit (1-20) with catgeory  
-        if ($show <= count($category) && $show > 0) {
-            for ($i = 0; $i < count($category); $i++) {
+    // * handle show limit (1-20) with catgeory  **** 
+    // TODO ----- limit how much to show from products when show is given 
+    if ($show <= count($category) && $show > 0) {
+      // for ($i = 0; $i <= $show; $i++) {
+      //     $tit = $title[$i];
+      //     // $tit = $title[rand(0, count($title), -1)];
+      //     $ids = $id[$tit];
+      //     $desc = $description[$tit];
+      //     $img = $image[$tit];
+      //     $pri = $price[$tit];
+      //     $categ = $category[$tit];
 
-                $tit = $title[$i];
-                $ids = $id[$tit];
-                $desc = $description[$tit];
-                $img = $image[$tit];
-                $pri = $price[$tit];
-                $categ = $category[$tit];
+      //     // if (in_array('women', $category)) {
 
-                if ($categ === "women") {
-                    $product = array(
-                        "id" => $ids,
-                        "title" => $tit,
-                        "description" => $desc,
-                        "image" => $img,
-                        "price" => $pri,
-                        "category" => $categ,
-                    );
+      //     if ($categ === "women") {
+      //         $product = array(
+      //             "id" => $ids,
+      //             "title" => $tit,
+      //             "description" => $desc,
+      //             "image" => $img,
+      //             "price" => $pri,
+      //             "category" => $categ,
+      //         );
+      //         shuffle($products);
+      //         array_push($products, $product);
+      //     }
+      // }
+      for ($i = 0; $i < $show; $i++) {
+        // if ($category[$i] == 'men') {
+        $tit = $title[rand(0, count($title) - 1)];
+        $ids = $id[$tit];
+        $desc = $description[$tit];
+        $img = $image[$tit];
+        $pri = $price[$tit];
+        $categ = $category[$tit];
 
-                    array_push($products, $product);
-                    shuffle($products);
-                }
-            }
+        $product = array(
+          "id" => $ids,
+          "title" => $tit,
+          "description" => $desc,
+          "image" => $img,
+          "price" => $pri,
+          "category" => $categ,
+        );
+        if ($product['category'] == "women") {
+          // foreach ($category as $c) {
+          array_push($products, $product);
         }
+        // }
+        // }
+      }
     }
+  }
 }
 
 
